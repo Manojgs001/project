e4e567193412b0b52acefed8d99b6949d527c3a6
Manojgs001 <manojgs@outlook.com>
2025-06-14 11:49:32 +0530

    view1

--- app/views.py
+++ app/views.py
@@ -14,10 +14,24 @@ def index(request):
     
     api = requests.get(api_url).json()
     
+    
     temperature= api["main"]["temp"]
-    
     city=api["name"]
-    
     country = api['sys']['country']
+    wind_speed = api['wind']['speed']
+    humidity = api['main']['humidity']
+    name = api['name']
+    icon = api['weather'][0]['icon']
+    weather = api["weather"][0]['main']
+    description = api['weather'][0]['description']
     
-    return render(request,'index.html',{'temperature':temperature,'city':city,'country':country})
+    img_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
+
+    
+    # https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=b87770e0c526050cc7426cd23fa9cf28&units=metric
+
+    
+    
+    
+    
+    return render(request,'index.html',{'temperature':temperature, 'wind_speed':wind_speed, 'humidity':humidity, 'name':name, 'country':country,'img_url':img_url, 'weather':weather, 'description':description})
